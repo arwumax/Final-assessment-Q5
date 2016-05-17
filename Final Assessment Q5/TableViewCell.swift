5a0cba967bb7ec0e0727156afb68e8e95ccf1d7d
arwumax31@gmail.com <arwumax31@gmai.com>
2016-05-17 22:11:58 +0800

    finished table view cell

---
+++ Final Assessment Q5/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Final Assessment Q5
+//
+//  Created by WuKwok Ho on 17/5/2016.
+//  Copyright © 2016 Wu Kwok Ho. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
